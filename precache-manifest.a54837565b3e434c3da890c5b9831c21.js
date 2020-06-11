a1b776594a28365d04679f4fb3a3f94eccb8cc0d
Khush Jain <khush4080@gmail.com>
2020-06-11 13:20:07 +0530

    Updates

---
+++ precache-manifest.a54837565b3e434c3da890c5b9831c21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9506a7121592f3d5c933279333870972",
+    "url": "/COVID-19-tracker-react-app/index.html"
+  },
+  {
+    "revision": "eeadf10640c88bb0e443",
+    "url": "/COVID-19-tracker-react-app/static/css/main.cc1cc5ce.chunk.css"
+  },
+  {
+    "revision": "3e15abe7204fb1190102",
+    "url": "/COVID-19-tracker-react-app/static/js/2.5fe6f44a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-tracker-react-app/static/js/2.5fe6f44a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeadf10640c88bb0e443",
+    "url": "/COVID-19-tracker-react-app/static/js/main.454cfc0a.chunk.js"
+  },
+  {
+    "revision": "9165ae0e9d4d7fccfe17",
+    "url": "/COVID-19-tracker-react-app/static/js/runtime-main.c3bfe982.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-tracker-react-app/static/media/image.d7265326.png"
+  }
+]);
